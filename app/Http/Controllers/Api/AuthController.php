24c43cd439506461561673a11dc652475c9eb78d
KomaCraft12 <komaromijano2002@gmail.com>
2025-02-24 11:45:45 +0100

    2FA 1.0

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,6 +10,10 @@ use Illuminate\Validation\ValidationException;
 // user model
 use App\Models\User;
 
+use PragmaRX\Google2FAQRCode\Google2FA;
+use PragmaRX\Google2FAQRCode\QRCode\Bacon;
+use PragmaRX\Google2FA\Exceptions\Google2FAException;
+
 class AuthController extends Controller
 {
 
@@ -18,16 +22,16 @@ class AuthController extends Controller
         try {
             $validatedData = $request->validate([
                 'fullname' => ['required', 'string', 'max:255'],
-                "username" => ["required", "string","max:255", 'unique:users'],
+                "username" => ["required", "string", "max:255", 'unique:users'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'phone_number' => ['required', 'string','max:20', "unique:users"],
+                'phone_number' => ['required', 'string', 'max:20', "unique:users"],
                 'birthdate' => ['required', 'string'],
                 'password' => ['required', 'string', 'min:8', 'confirmed'],
             ]);
 
             $user = User::create([
                 'fullname' => $validatedData['fullname'],
-                'username'=> $validatedData['username'],
+                'username' => $validatedData['username'],
                 'email' => $validatedData['email'],
                 'phone_number' => $validatedData['phone_number'],
                 'birthdate' => $validatedData['birthdate'],
@@ -52,6 +56,16 @@ class AuthController extends Controller
         }
     }
 
+    public function auth($user)
+    {
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'access_token' => $token,
+        ], 200);
+    }
+
     public function login(Request $request)
     {
         try {
@@ -68,13 +82,16 @@ class AuthController extends Controller
             }
 
             $user = Auth::user();
-            $token = $user->createToken('auth_token')->plainTextToken;
 
-            return response()->json([
-                'status' => 'success',
-                'user' => $user,
-                'access_token' => $token,
-            ], 200);
+            if ($user->two_factor_secret == null) {
+                return $this->auth($user);
+            } else {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'You need 2FA authentication!',
+                    '2fa' => true,
+                ], 200);
+            }
 
         } catch (ValidationException $e) {
             return response()->json([
@@ -83,6 +100,76 @@ class AuthController extends Controller
                 'errors' => $e->errors(),
             ], 422);
         }
+    }
+
+    public function verify_2FA(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        Auth::attempt($credentials);
+
+        $user = Auth::user();
+        $google2fa = new Google2FA();
+
+        $valid = $google2fa->verifyKey(
+            $user->two_factor_secret,
+            $request->two_factor_code
+        );
+
+        if ($valid) {
+            return $this->auth($user);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid 2FA code'
+            ], 401);
+        }
+    }
+
+    public function activate_2FA()
+    {
+        try {
+            $user = Auth::user();
+            $google2fa = new Google2FA(new Bacon());
+            $secretKey = $google2fa->generateSecretKey();
+
+            $user->two_factor_secret = $secretKey;
+            $user->save();
+
+            $qr_image = $google2fa->getQRCodeInline(
+                config('app.name'),
+                $user->email,
+                $secretKey
+            );
+
+            return response()->json([
+                'status' => 'success',
+                'message' => '2FA has been activated',
+                'secret_key' => $secretKey,
+                'qr_image' => $qr_image
+            ], 200);
+
+        } catch (Google2FAException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error',
+            ], 0);
+        }
+    }
+
+    public function deactivate_2FA()
+    {
+        $user = Auth::user();
+        $user->two_factor_secret = null;
+        $user->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Two Factor Authentacation successfull deactivated!',
+        ], 200);
     }
 
     public function logout(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login')->name('login');
     Route::post('register', 'register')->name('register');
+    Route::post('2fa/verify', 'verify_2FA')->name('verify_2FA');
     //Route::post('login', 'postLogin')->name('login.post');
     Route::get('logout', 'logout')->name('logout')->middleware('auth:sanctum');
+    Route::get('2fa/activate', 'activate_2FA')->name('activate_2FA')->middleware('auth:sanctum');
+    Route::get('2fa/deactivate', 'deactivate_2FA')->name('deactivate_2FA')->middleware('auth:sanctum');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
